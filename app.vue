c7a1a52d873701eb88293aead1ca82741fca5d9e
railombardi <raidslombardi@gmail.com>
2023-10-27 02:24:36 -0300

    feat: :sparkles: configure initial layout and header

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="px-6 bg-[#1E28320D] h-[100vh]">
+    <div>
+      <FsHeader/>
+    </div>
+    <slot/>
+  </div>
+</template>
+
+<script>
+import FsHeader from '~/components/FsHeader.vue';
+
+export default {
+  components: { FsHeader }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
