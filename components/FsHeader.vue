b6b1d44284aa9d4933d2d34d318bce5d320a5426
railombardi <raidslombardi@gmail.com>
2023-10-27 02:42:17 -0300

    feat: :sparkles: add products categories request

--- components/FsHeader.vue
+++ components/FsHeader.vue
@@ -12,55 +12,37 @@
   <div class="w-full h-[1px] bg-[#E3E3E3]"/>
   <nav class="hidden lg:block">
     <ul class="flex flex-wrap justify-between pt-4">
-      <li v-for="category in 7" :key="category">
-        <a> Lorem ipsum </a>
+      <li v-for="category in categories" :key="category">
+        <a> {{ category }} </a>
       </li>
     </ul>
   </nav>
 </header>
-<!-- <header>
-  <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
-    <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-      <a href="#" class="flex items-center">
-        <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Fake Store</span>
-      </a>
-      <div class="flex items-center lg:order-2">
-        <button @click="toggleMenu" type="button" class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-expanded="false">
-            <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
-            <svg class="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-        </button>
-      </div>
-      <div v-show="showMenu" class="justify-between items-center w-full lg:flex lg:w-auto lg:order-1">
-        <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-          <li>
-            <a href="#" class="block py-2 pr-4 pl-3 text-white rounded bg-blue-700 lg:bg-transparent lg:text-blue-700 lg:p-0 dark:text-white" aria-current="page">Home</a>
-          </li>
-          <li>
-            <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Produtos</a>
-          </li>
-          <li>
-            <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Quem somos</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-    </header> -->
 </template>
 
 <script>
-  export default {
-    data(){
-      return {
-        showMenu: false
-      }
+export default {
+  data(){
+    return {
+      showMenu: false,
+      categories: []
+    }
+  },
+  created(){
+    this.getAllCategories()
+  },
+  methods: {
+    toggleMenu(){
+      this.showMenu = !this.showMenu
     },
-    methods: {
-      toggleMenu(){
-        this.showMenu = !this.showMenu
-      }
+    async getAllCategories() {
+      await this.$axios.get('/products/categories').then(response => {
+        console.log(response)
+        this.categories = response.data
+      })
     }
   }
+}
 </script>
 
 <style lang="scss" scoped>
